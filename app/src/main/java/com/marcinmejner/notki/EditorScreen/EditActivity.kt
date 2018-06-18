d0ef39f1c8fffb97586d1d97523439120c8cff94
Marcin Mejner <marczu09@gmail.com>
2018-06-18 16:42:39 +0200

    commit 6

--- app/src/main/java/com/marcinmejner/notki/EditorScreen/EditActivity.kt
+++ app/src/main/java/com/marcinmejner/notki/EditorScreen/EditActivity.kt
@@ -1,21 +1,30 @@
 package com.marcinmejner.notki.EditorScreen
 
-import android.annotation.SuppressLint
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
 import com.marcinmejner.notki.R
 import kotlinx.android.synthetic.main.activity_edit.*
 
 class EditActivity : AppCompatActivity() {
 
-    @SuppressLint("RestrictedApi")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit)
         setSupportActionBar(toolbar)
-        supportActionBar?.setDefaultDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
 
 
+
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item?.itemId == android.R.id.home) {
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/com/marcinmejner/notki/MainScreen/MainActivity.kt
+++ app/src/main/java/com/marcinmejner/notki/MainScreen/MainActivity.kt
@@ -62,6 +62,7 @@ class MainActivity : AppCompatActivity() {
         fab.setOnClickListener { view ->
           Intent(this@MainActivity, EditActivity::class.java).apply {
               startActivity(this)
+
           }
         }
     }
