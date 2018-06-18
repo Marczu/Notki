ec116e4d8ab14dc56f1b4777c90a9bd230c99122
Marcin Mejner <marczu09@gmail.com>
2018-06-18 13:21:18 +0200

    commit 1

--- app/src/main/java/com/marcinmejner/notki/MainActivity.kt
+++ app/src/main/java/com/marcinmejner/notki/MainActivity.kt
@@ -3,24 +3,29 @@ package com.marcinmejner.notki
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.RecyclerView
 import android.view.Menu
 import android.view.MenuItem
 import com.marcinmejner.notki.R.id.fab
 import com.marcinmejner.notki.R.id.toolbar
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
+
+    var recyclerView: RecyclerView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+        init()
+
+
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -30,12 +35,27 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+
         return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
     }
+
+    fun init(){
+        initWidgets()
+        initFab()
+        }
+
+    private fun initFab() {
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+    }
+
+    private fun initWidgets() {
+        recyclerView = recycler_view
+    }
 }
+
