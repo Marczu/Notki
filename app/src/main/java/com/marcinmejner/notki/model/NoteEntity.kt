b3fd944bfa840e9f15244d23812f9f817f465d70
Marcin Mejner <marczu09@gmail.com>
2018-06-18 16:55:46 +0200

    commit 7 budowanie room

--- app/src/main/java/com/marcinmejner/notki/model/NoteEntity.kt
+++ app/src/main/java/com/marcinmejner/notki/model/NoteEntity.kt
@@ -1,8 +1,12 @@
 package com.marcinmejner.notki.model
 
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
 import java.util.*
 
+@Entity(tableName = "notes")
 data class NoteEntity(
+        @PrimaryKey(autoGenerate = true)
         var date: Date? = null,
         var noteText:String = "",
         var id:Int = 0
